6a63783b0d0c1c1a3191037b0025f75c3f95ff1c
bdshanto <bdshanto@hotmail.com>
2021-05-08 15:45:47 +0600

    child parent added

---
+++ problem-module-and-solve/ChildParent/ChildParent/Combo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using ChildParent;
+using static System.Console;
+
+namespace Try
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            long givenValue = 50000;
+            long valueForEachNode = 1500;
+            long maxChildOfEachNode = 3;
+
+            var totalNodeCount = Convert.ToInt64(Math.Floor((double)(givenValue / valueForEachNode)));
+
+            IndependentNodes(totalNodeCount, out var singleNodes, valueForEachNode);
+            AssignThroughNodes(singleNodes, out var result, maxChildOfEachNode);
+
+        }
+
+        private static void AssignThroughNodes(IReadOnlyCollection<NmLeaderSrM> singleNodes, out List<NmLeaderSrM> result, long maxChildOfEachNode)
+        {
+            result = new List<NmLeaderSrM>();
+            var currentRootCounter = 1; // always start from index 1 or first id
+            var id = currentRootCounter + 1; // leave first node through all
+            var currentRootParent = singleNodes.First();
+
+            while (id <= singleNodes.Count)
+            {
+                var getSingleNode = singleNodes.FirstOrDefault(sn => sn.Id == id);
+                if (getSingleNode is not null && currentRootParent is not null)
+                {
+                    id++;
+                    getSingleNode.IsUpNodeConnected = true;
+                    getSingleNode.PlacementId = currentRootParent.Id;
+                    currentRootParent?.Associates.Add(getSingleNode);
+                }
+
+                if (currentRootParent?.Associates.Count != maxChildOfEachNode) continue;
+                {
+                    currentRootCounter++;
+                    currentRootParent.IsDownNodeConnected = true;
+                    result.Add(currentRootParent);
+                    currentRootParent = singleNodes.FirstOrDefault(sn => sn.Id == currentRootCounter);
+                }
+            }
+
+            result.Add(currentRootParent);
+        }
+
+        private static void IndependentNodes(long loopThrough, out List<NmLeaderSrM> singleNodes, long valueForEachNode)
+        {
+            singleNodes = new List<NmLeaderSrM>();
+            // Single increment
+            for (var i = 1; i <= loopThrough; i++)
+            {
+                singleNodes.Add(new NmLeaderSrM
+                {
+                    Id = i,
+                    Unit = valueForEachNode,
+                    Associates = new List<NmLeaderSrM>()
+                });
+            }
+
+            var isLastValueContain = singleNodes.Any(n => n.Id == loopThrough);
+            if (!isLastValueContain)
+            {
+                singleNodes.Add(
+                    new NmLeaderSrM { Id = loopThrough, Unit = valueForEachNode, Associates = new List<NmLeaderSrM>() });
+            }
+        }
+
+        /*public record Node
+        {
+            public long Id { get; set; }
+            public long ParentId { get; set; }
+            public long Value { get; set; }
+            public bool IsUpNodeConnected { get; set; }
+            public bool IsDownNodeConnected { get; set; }
+            public List<Node> Nodes { get; set; }
+            public bool IsRootNode { get; set; }
+
+            public Node()
+            {
+                Nodes = new List<Node>();
+            }
+        }*/
+    }
+}
---
+++ problem-module-and-solve/ChildParent/WebApplication2/NmLeaderSrM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2
+{
+    public class NmLeaderSrM
+    {
+        public NmLeaderSrM()
+        {
+            Associates  = new List<NmLeaderSrM>();
+        }
+        public long Id { get; set; }
+        [NotMapped]
+        public long loopThroughId { get; set; }
+        public string Name { get; set; }
+        public string MobileNo { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhotoUrl { get; set; }
+        public string CoverPhotoUrl { get; set; }
+        public string Description { get; set; }
+        public string AutoGenCode { get; set; }
+        public long? ReferralId { get; set; } // who is referring 
+        public long? PlacementId { get; set; } // immediate parent
+        public long UserId { get; set; }
+
+        public DateTime JoiningDate { get; set; }
+
+
+        //------------
+        public string ClosingStatementCode { get; set; } // AutoGenCode, Another Class
+        public string RankId { get; set; } // such as, *, **, *** etc.  we will move this to another class
+        public double Unit { get; set; } // such as, 1500 etc.  we will move this to another class
+
+        public double ReferralBonus { get; set; }
+        public double GenerationBonus { get; set; }
+        public double RankBonus { get; set; }
+        public long OrganizationId { get; set; }
+
+
+        public virtual NmLeaderSrM Referral { get; set; }
+        public virtual NmLeaderSrM Placement { get; set; }
+
+        public virtual ICollection<NmLeaderSrM> Associates { get; set; } = new List<NmLeaderSrM>();
+
+        //not mapped models
+        public bool IsRootNode { get; set; }
+        public bool IsUpNodeConnected { get; set; }
+        public bool IsDownNodeConnected { get; set; }
+    }
+}
