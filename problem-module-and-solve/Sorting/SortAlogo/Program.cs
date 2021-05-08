034f7882e12f5022f0a5876f447b117d6939135f
bdshanto <bdshanto@hotmail.com>
2021-05-08 22:13:38 +0600

    bubble sort algo

--- problem-module-and-solve/Sorting/SortAlogo/Program.cs
+++ problem-module-and-solve/Sorting/SortAlogo/Program.cs
@@ -9,46 +9,39 @@ namespace SortAlogo
 
             int[] array = new[] { 3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48, 100, 20 };
 
-            BubbleSortUseNestedLoop(array, out var bubbleSortUseNestedLoop);
-            PrintLine("", bubbleSortUseNestedLoop);
+            BubbleSortUseNestedLoop(array);
+            PrintLine("Bubble Sort Use Nested Loop", array);
 
         }
 
-        private static void BubbleSortUseNestedLoop(int[] arr, out int[] array)
+        private static void BubbleSortUseNestedLoop(int[] arr)
         {
-            array = new int[] { };
-
             for (int write = 0; write < arr.Length; write++)
             {
                 for (int sort = 0; sort < arr.Length - 1; sort++)
                 {
-                    if (arr[sort] > arr[sort + 1])
-                    {
-                        var temp = arr[sort + 1];
-                        arr[sort + 1] = arr[sort];
-                        arr[sort] = temp;
-                    }
+                    if (arr[sort] > arr[sort + 1]) Swapping(arr, write, sort);
                 }
             }
 
-            array = arr;
-            ReadKey();
-
         }
 
-        private static void BubbleSort()
+        static void Swapping(int[] arr, int first, int next)
         {
+            var temp = arr[first];
+            arr[first] = arr[next];
+            arr[first] = temp;
 
         }
-
-        static void PrintLine(string sortName, int[] args)
+        
+        static void PrintLine(string sortName, params int[] args)
         {
-            WriteLine(sortName);
+            WriteLine(sortName + "\n");
             for (int i = 0; i < args?.Length; i++)
             {
                 Write($"{args[i]}, ");
             }
-            WriteLine("------------------------->\n");
+            WriteLine("\n------------------------->\n");
 
             ReadKey();
         }
